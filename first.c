b391414268c88e99fd51ab495e8d901acceb95b2
root <root@upmauro.(none)>
2009-07-03 17:30:59 -0300

    Teste de rotacao de label em group como container.

--- first.c
+++ first.c
@@ -17,63 +17,64 @@
 #include <clutter/clutter.h>
 #include <stdlib.h>
 
+ClutterText *label_angulo_atual;
+
 struct RotationClosure {
     ClutterActor *actor;
     CoglFixed final_angle;
     CoglFixed current_angle;
 };
 
+
 static gboolean rotate_square(gpointer data)
 {
     struct RotationClosure *clos = data;
-    
     clutter_actor_set_rotation(clos->actor, CLUTTER_X_AXIS, clos->current_angle, 0, 0, 0);
+
     clos->current_angle += COGL_FIXED_1;
+	clutter_text_set_text(label_angulo_atual,"aiuahaui");
 
     if(clos->current_angle == clos->final_angle)
-	return FALSE;
+	    return FALSE;
 
     return TRUE;
 }
 
+
+
+
 int main(int argc, char *argv[])
 {
-  ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff };
-  ClutterColor actor_color = { 0xff, 0xff, 0xff, 0x99 };
-    struct RotationClosure clos;
-  clutter_init (&argc, &argv);
+	ClutterColor stage_color = { 0x00, 0x00, 0x00, 0xff };
+	ClutterColor actor_color = { 0xff, 0xff, 0xff, 0x99 };
 
-  /* Get the stage and set its size and color: */
-  ClutterActor *stage = clutter_stage_get_default ();
-  clutter_actor_set_size (stage, 800, 600);
-  clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
+	clutter_init (&argc, &argv);
 
-  /* Add a rectangle to the stage: */
-  ClutterActor *rect = clutter_rectangle_new_with_color (&actor_color);
-  clutter_actor_set_size (rect, 100, 100);
-  clutter_actor_set_position (rect, 20, 20);
-  clutter_container_add_actor (CLUTTER_CONTAINER (stage), rect);
-  clutter_actor_show (rect);
+	/* Get the stage and set its size and color: */
+	ClutterActor *stage = clutter_stage_get_default ();
+	clutter_actor_set_size (stage, 800, 600);
+	clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
 
-  /* Add a label to the stage: */
-  ClutterActor *label = clutter_text_new_full ("Sans 12", "Some Text", &actor_color);
-  clutter_actor_set_size (label, 500, 500);
-  clutter_actor_set_position (label, 20, 150);
-  clutter_container_add_actor (CLUTTER_CONTAINER (stage), label);
-  clutter_actor_show (label);
 
-    clos.actor = rect;
-    clos.final_angle = COGL_FIXED_FROM_FLOAT(360.0);
-    clos.current_angle = 0;
 
-    g_timeout_add(1000/360, rotate_square, &clos);
-  
-  /* Show the stage: */
-  clutter_actor_show (stage);
+	ClutterActor *painel_central = clutter_group_new ();
+  	clutter_actor_set_position (painel_central, 400, 300);
+  	clutter_container_add_actor (CLUTTER_CONTAINER (stage), painel_central);
+  	clutter_actor_show (painel_central);
 
-  /* Start the main loop, so we can respond to events: */
-  clutter_main ();
+	g_timeout_add(100, rotate_square, &painel_central);
 
-  return EXIT_SUCCESS;
+	/* Show the stage: */
+	clutter_actor_show (stage);
+
+
+
+
+
+
+	/* Start the main loop, so we can respond to events: */
+	clutter_main ();
+
+	return EXIT_SUCCESS;
 
 }
